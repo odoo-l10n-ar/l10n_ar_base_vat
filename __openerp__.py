7fd04cf781616aa893cfdade33506ca432965273
Cristian Sebastian Rocha <crocha@kekkaishi.jjdiazarana.net>
2012-07-16 15:46:25 -0300

    Actualizamos la autoria de base_vat para la localizacion argentina.

--- __openerp__.py
+++ __openerp__.py
@@ -1,35 +1,35 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-#    Copyright (C) 2008-2011  Luis Falcon
+# Copyright (C) 2012 OpenERP - Team de Localización Argentina.
+# https://launchpad.net/~openerp-l10n-ar-localization
 #
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 #
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
 #
-#    You should have received a copy of the GNU General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-
 {
     'name': 'VAT Localization Argentina',
-    'version': '2.0',
-    'author': 'Thymbra - Torre de Hanoi',
-    'category': 'Localisation/Base',
-    'website': 'http://www.thymbra.com/',
+    'version': '2.1',
+    'author':   'OpenERP - Team de Localización Argentina',
+    'category': 'Localization/Argentina',
+    'website':  'https://launchpad.net/~openerp-l10n-ar-localization',
     'license': 'GPL-3',
     'description': """
-Localization Model of Argentina
-Includes:
- - Added check the validity of that VAT Number for Argentina
+Función de validación del CUIT según la AFIP.
 
+Incluye:
+ - Función de validación del CUIT según la AFIP.
 """,
     'depends': [
         'base_vat',
@@ -38,3 +38,4 @@ Includes:
     'active': False,
     'installable': True,
 }
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
